36c4f8fbeebdc7e83932fb58ad18d798caf3f6e9
hahoho87 <hahoho87@gmail.com>
2021-04-24 20:44:38 +0900

    Initial commit

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `hahoho blog`,
+  description: `공부하자 열심히 하자 꾸준히 하자`,
+  author: `hahoho87`,
+  introduction: `개발 잘하고 싶다`,
+  siteUrl: `https://hahoho87.github.io`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: ``, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    utterances: 'hahoho87/blog-comments', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'hahoho87',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+}
